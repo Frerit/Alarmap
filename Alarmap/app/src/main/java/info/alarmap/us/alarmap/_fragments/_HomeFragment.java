fe6d7f817ff6fc36ecc37bcbaf90564d9d5ec3e9
Julian Perez <freritmg12@gmail.com>
2017-06-13 16:01:03 -0500

    Fragmentos

---
+++ Alarmap/app/src/main/java/info/alarmap/us/alarmap/_fragments/_HomeFragment.java
@@ -0,0 +1,30 @@
+package info.alarmap.us.alarmap._fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import info.alarmap.us.alarmap.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class _HomeFragment extends Fragment {
+
+
+    public _HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
